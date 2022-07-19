32ddad723deca120ecfaa3fb383a193c2a6ac7ff
VeroVelazco <veroverona31@gmail.com>
2022-07-18 22:26:07 -0300

    add flexbox

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = 3030;
 app.use(express.static('public'));
 
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'home.html')));
